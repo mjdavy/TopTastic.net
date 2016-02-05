5df6f9f41cbd25d7c96d900d335039312c0dcc00
mjdavy <mjdavy@hotmail.com>
2016-02-05 17:54:59 -0500

    Added back echonest artist tests

---
+++ TopTastic.tests/EchoNestArtistTests.cs
@@ -0,0 +1,23 @@
+﻿using EchoNest;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace TopTastic.tests
+{
+    [TestClass]
+    public class EchoNestArtistTests : EchoNestTests
+    {
+        [TestMethod]
+        public void TestArtistBio()
+        {
+            using (var session = new EchoNestSession(ApiKey))
+            {
+                string query = "Adele";
+                var result = session.Query<EchoNest.Artist.Biography>().Execute(query, numberOfResults: 1);
+                var expected = 1;
+                var actual = result.Biographies.Count;
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+    }
+}
